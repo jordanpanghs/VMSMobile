7d4a93eef91aa8f0df396b24e9f1e09a4a2f6f25
Jordan <wild_hunter0806@hotmail.com>
2023-07-24 21:43:37 +0800

    Installed react native icons

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,9 +1,17 @@
 import { Tabs } from "expo-router";
+import Icon from "react-native-vector-icons/Ionicons";
 
 export default () => {
   return (
     <Tabs>
-      <Tabs.Screen name="home" />
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarIcon: () => {
+            return <Icon name="home" size={20} color={"black"} />;
+          },
+        }}
+      />
       <Tabs.Screen name="list" />
     </Tabs>
   );
