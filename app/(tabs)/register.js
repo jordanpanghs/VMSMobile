00f60c4263999e211794aee2d4ec9336de7e1359
Jordan <wild_hunter0806@hotmail.com>
2023-08-11 16:32:15 +0800

    Added switch button for register page

--- app/(tabs)/register.js
+++ app/(tabs)/register.js
@@ -1,10 +1,47 @@
-import { Text, View } from "react-native";
-import React, { Component } from "react";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import React, { Component, useState } from "react";
+import SwitchSelector from "react-native-switch-selector";
 
 export default function Register() {
+  const [registerSelection, setRegisterSelection] = useState("");
+
+  const handleSelection = (selection) => {
+    setRegisterSelection(selection);
+  };
+
+  const options = [
+    { label: "Visitor", value: "visitor" },
+    { label: "Parcel", value: "parcel" },
+  ];
+
   return (
-    <View>
-      <Text>Register</Text>
+    <View style={styles.container}>
+      <Text>{registerSelection}</Text>
+      <SwitchSelector
+        options={options}
+        initial={0}
+        style={styles.floatingButton}
+        buttonColor={"#000"}
+        borderRadius={5}
+        hasPadding
+        onPress={(value) => setRegisterSelection(value)}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  floatingButton: {
+    position: "absolute",
+    bottom: 20,
+    width: "90%",
+    height: "auto",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
