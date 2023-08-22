002e24650fb9b1d018d37bec4de938f3edf6beca
Jordan <wild_hunter0806@hotmail.com>
2023-08-22 23:53:04 +0800

    Added register parcel with scanner feature

---
+++ app/(tabs)/Register/findparcel.js
@@ -0,0 +1,209 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { useLocalSearchParams, useRouter } from "expo-router";
+
+import {
+  collection,
+  getDoc,
+  doc,
+  updateDoc,
+  getDocs,
+  query,
+  where,
+  collectionGroup,
+} from "firebase/firestore";
+
+import { db } from "../../../firebase";
+
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+export default findParcel = () => {
+  const [parcelData, setParcelData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [documentRef, setDocumentRef] = useState("");
+
+  const params = useLocalSearchParams();
+  const parcelTrackingNumber = params.qrData;
+
+  const router = useRouter();
+
+  useEffect(() => {
+    try {
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  fetchData = async () => {
+    try {
+      const q = query(
+        collectionGroup(db, "userRegisteredParcels"),
+        where("parcelTrackingNumber", "==", parcelTrackingNumber)
+      );
+
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot.size > 0) {
+        const docData = querySnapshot.docs.map((doc) => ({
+          docRef: doc.ref,
+          ...doc.data(),
+        }));
+        setParcelData(docData[0]);
+        setDocumentRef(docData[0].docRef);
+        setIsLoading(false);
+      } else {
+        alert("No such document!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  handleRegisterParcel = async () => {
+    if (parcelData.hasArrived) {
+      router.back();
+      alert("Parcel has already been registered!");
+      return;
+    }
+    setIsLoading(true);
+
+    try {
+      await updateDoc(documentRef, {
+        hasArrived: true,
+        arrivalTime: new Date().toISOString(),
+      });
+      setIsLoading(false);
+      router.back();
+      alert("Parcel registered!");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.textContainer}>
+          <Text style={styles.textLabel}>{"Parcel Tracking Number"}</Text>
+          <Text style={styles.text}>{parcelData.parcelTrackingNumber}</Text>
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.textLabel}>{"Parcel Receiver Name:"}</Text>
+          <Text style={styles.text}>{parcelData.parcelReceiverName}</Text>
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.textLabel}>
+            {"Parcel Receiver Telephone Number:"}
+          </Text>
+          <Text style={styles.text}>{parcelData.parcelReceiverTelNo}</Text>
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.textLabel}>
+            {"Parcel Receiver Resident Unit"}
+          </Text>
+          <Text style={styles.text}>{parcelData.parcelReceiverUnit}</Text>
+        </View>
+
+        <View style={styles.confirmationContainer}>
+          <View>
+            <Text style={styles.confirmationText}>
+              Confirm Registering Parcel?
+            </Text>
+          </View>
+
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.buttonCancel}
+              onPress={() => router.back()}
+            >
+              <Ionicons name="close" size={24} color="white" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.buttonConfirm}
+              onPress={() => handleRegisterParcel()}
+            >
+              <Ionicons name="checkmark" size={24} color="white" />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {isLoading && (
+          <View
+            style={[
+              StyleSheet.absoluteFill,
+              {
+                backgroundColor: "rgba(0,0,0,0.4)",
+                alignItems: "center",
+                justifyContent: "center",
+              },
+            ]}
+          >
+            <ActivityIndicator color="#fff" animating size="large" />
+          </View>
+        )}
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 20,
+    padding: 20,
+  },
+  textContainer: {
+    flexDirection: "column",
+  },
+  text: {
+    fontSize: 20,
+    fontFamily: "DMRegular",
+    color: "#007AFF",
+  },
+  textLabel: {
+    fontSize: 20,
+    fontFamily: "DMRegular",
+  },
+  confirmationContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20,
+  },
+  buttonContainer: {
+    justifyContent: "space-evenly",
+    flexDirection: "row",
+    gap: 50,
+  },
+  buttonConfirm: {
+    backgroundColor: "#007AFF",
+    borderRadius: 5,
+    padding: 10,
+    alignItems: "center",
+    gap: 5,
+  },
+  buttonCancel: {
+    backgroundColor: "red",
+    borderRadius: 5,
+    padding: 10,
+    alignItems: "center",
+    gap: 5,
+  },
+  confirmationText: {
+    fontSize: 20,
+    fontFamily: "DMBold",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontFamily: "DMBold",
+  },
+});
--- components/security/register/RegisterParcel.js
+++ components/security/register/RegisterParcel.js
@@ -1,269 +1,105 @@
 import React, { useState, useEffect } from "react";
-import {
-  Button,
-  Image,
-  View,
-  StyleSheet,
-  ActivityIndicator,
-  SafeAreaView,
-  Text,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
-import * as ImagePicker from "expo-image-picker";
-import * as FileSystem from "expo-file-system";
+import { Text, View, StyleSheet, Button, TouchableOpacity } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { useRouter } from "expo-router";
+import { set } from "react-native-reanimated";
 
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-import axios from "axios";
-
-import {
-  getStorage,
-  ref,
-  uploadString,
-  getDownloadURL,
-  uploadBytes,
-} from "firebase/storage";
-
-const imgDir = FileSystem.documentDirectory + "images/";
-
-const ensureDirExists = async () => {
-  const dirInfo = await FileSystem.getInfoAsync(imgDir);
-  if (!dirInfo.exists) {
-    await FileSystem.makeDirectoryAsync(imgDir, { intermediates: true });
-  }
-};
-
 export default function RegisterParcel() {
-  const [uploading, setUploading] = useState(false);
-  const [image, setImage] = useState("");
-  const [fileURL, setfileURL] = useState("");
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [toggleQRCamera, setToggleQRCamera] = useState(false);
 
-  // Load images on startup
+  const router = useRouter();
+
   useEffect(() => {
-    loadImages();
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getBarCodeScannerPermissions();
   }, []);
 
-  // Load images from file system
-  const loadImages = async () => {
-    await ensureDirExists();
-    const files = await FileSystem.readDirectoryAsync(imgDir);
-    if (files.length > 0) {
-      setImages(files.map((f) => imgDir + f));
-    }
+  const handleToggleQRCamera = () => {
+    setScanned(false);
+    setToggleQRCamera(!toggleQRCamera);
   };
 
-  // Select image from library or camera
-  const selectImage = async (useLibrary) => {
-    let result;
-    const options = {
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 0.75,
-    };
+  const handleBarCodeScanned = ({ data }) => {
+    setScanned(true);
 
-    if (useLibrary) {
-      result = await ImagePicker.launchImageLibraryAsync(options);
-    } else {
-      await ImagePicker.requestCameraPermissionsAsync();
-      result = await ImagePicker.launchCameraAsync(options);
-    }
-
-    // Save image if not cancelled
-    if (!result.canceled) {
-      saveImage(result.assets[0].uri);
-    }
-  };
-
-  // Save image to file system
-  const saveImage = async (uri) => {
-    // await ensureDirExists();
-    // const filename = new Date().getTime() + ".jpeg";
-    // const dest = imgDir + filename;
-    // await FileSystem.copyAsync({ from: uri, to: dest });
-    setfileURL(uri); //Test function
-    setImage(uri);
-  };
-
-  // Upload image to google fire storage
-  const uploadImage = async (uri) => {
-    setUploading(true);
-
-    const response = await fetch(uri);
-    const blob = await response.blob();
-    const filename = uri.split("/").pop();
-
-    const storage = getStorage();
-    const storageRef = ref(storage, `visitorCarPlate/${filename}`);
-    setFileName(storageRef);
-
-    try {
-      await uploadBytes(storageRef, blob);
-      alert("Image uploaded successfully");
-    } catch (e) {
-      console.log(e);
-    }
-
-    setUploading(false);
-  };
-
-  const registerParcel = async (fileURL) => {
-    const resultsArray = await detectLabels(fileURL);
-
-    // const trackingNumberRegex = /^(SPXMY|MY|SPX|SPE|\d{1})\d{7,14}.$/; // Regular expression to match 12-digit tracking number
-    // const trackingNumber = resultsArray.find((text) =>
-    //   trackingNumberRegex.test(text)
-    // );
-    console.log(resultsArray);
-  };
-
-  const detectLabels = async (fileURL) => {
-    if (!fileURL) {
-      alert("No image detected. Please upload or capture an image first.");
-      return [];
-    }
-
-    const credentials = require("../../../visitor-management-syste-3f0f7-e8395bfdb89d.json");
-    const apiKey = credentials.private_key;
-    const url = `https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCxKvcfZcIQy42hjIwbo8jominxkAW6-J0`;
-
-    //Convert latest upload to base64 image for API call
-    const base64Data = await FileSystem.readAsStringAsync(fileURL, {
-      encoding: FileSystem.EncodingType.Base64,
+    router.push({
+      pathname: "/register/findparcel",
+      params: {
+        qrData: data,
+      },
     });
-
-    const requestBody = {
-      requests: [
-        {
-          image: {
-            content: base64Data,
-          },
-          features: [
-            {
-              type: "TEXT_DETECTION",
-              maxResults: 10,
-            },
-          ],
-        },
-      ],
-    };
-
-    try {
-      const response = await axios.post(url, requestBody);
-      const texts = response.data.responses[0].textAnnotations.map(
-        (texts) => texts.description
-      );
-      return texts;
-    } catch (error) {
-      console.log(error);
-      return [];
-    }
   };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
   return (
-    <SafeAreaView style={{ flex: 1, padding: 20 }}>
-      <View style={{ flex: 1 }}>
-        <Text
+    <View style={styles.container}>
+      {!toggleQRCamera && (
+        <View
           style={{
-            textAlign: "center",
-            fontSize: 20,
-            fontWeight: "500",
+            justifyContent: "center",
+            alignItems: "center",
+            opacity: 0.2,
+            height: "80%",
             paddingTop: 50,
           }}
         >
-          Select or Upload Image of Parcel Label
-        </Text>
-
-        {image && (
-          <View style={{ flex: 1 }}>
-            {image && (
-              <Image
-                source={{ uri: image }}
-                alt="No Image Selected"
-                style={{ flex: 1, resizeMode: "contain" }}
-              />
-            )}
-          </View>
-        )}
-
-        {!image && (
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              opacity: 0.2,
-            }}
-          >
-            <MaterialCommunityIcons
-              name="image-off-outline"
-              size={200}
-              color="black"
-            />
-          </View>
-        )}
-      </View>
-
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-evenly",
-          marginVertical: 20,
-        }}
-      >
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => selectImage(true)}
-        >
-          <Text style={styles.buttonText}>Upload Image </Text>
           <MaterialCommunityIcons
-            name="image-multiple"
-            size={24}
-            color="white"
+            name="barcode-scan"
+            size={180}
+            color="black"
           />
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => selectImage(false)}
-        >
-          <Text style={styles.buttonText}>Capture Image </Text>
-          <MaterialCommunityIcons name="camera" size={24} color="white" />
-        </TouchableOpacity>
-      </View>
-
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => registerParcel(fileURL)}
-      >
-        <Text style={styles.buttonText}>Click Me </Text>
-      </TouchableOpacity>
-
-      {uploading && (
-        <View
-          style={[
-            StyleSheet.absoluteFill,
-            {
-              backgroundColor: "rgba(0,0,0,0.4)",
-              alignItems: "center",
-              justifyContent: "center",
-            },
-          ]}
-        >
-          <ActivityIndicator color="#fff" animating size="large" />
         </View>
       )}
-    </SafeAreaView>
+
+      <View style={styles.qrcameracontainer}>
+        {toggleQRCamera && (
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+        )}
+      </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => handleToggleQRCamera()}
+      >
+        <Text style={styles.buttonText}>
+          {toggleQRCamera ? "Cancel" : "Scan QR Code"}
+        </Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  qrcameracontainer: {
+    flex: 1,
+  },
   button: {
+    margin: 20,
     backgroundColor: "#007AFF",
     borderRadius: 5,
     padding: 10,
     alignItems: "center",
-    gap: 5,
   },
   buttonText: {
     color: "#fff",
