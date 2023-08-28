4d3049697b6881cf9cd4aa7495b97a11fcc8368e
Jordan <wild_hunter0806@hotmail.com>
2023-08-28 19:50:09 +0800

    Moved notification listeners to profile indexjs

--- app/(tabs)/Profile/index.js
+++ app/(tabs)/Profile/index.js
@@ -1,10 +1,54 @@
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
-import React, { Component } from "react";
+import React, { useState, useEffect, useRef } from "react";
+
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 
 import { useAuth } from "../../../context/AuthContext";
 
+import { useRouter } from "expo-router";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: true,
+  }),
+});
+
 export default function Profile() {
   const { logout, currentUser, userIsSecurity, userResidentUnit } = useAuth();
+
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+        router.push("visits");
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   function handleLogOut() {
     logout();
@@ -33,6 +77,10 @@ export default function Profile() {
             <Text style={styles.text}>{userResidentUnit}</Text>
           </View>
         )}
+
+        <View style={styles.unitTextContainer}>
+          <Text>{expoPushToken}</Text>
+        </View>
       </View>
 
       <TouchableOpacity style={styles.button} onPress={() => handleLogOut()}>
@@ -40,6 +88,43 @@ export default function Profile() {
       </TouchableOpacity>
     </View>
   );
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  if (Device.isDevice) {
+    try {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } catch (e) {
+      console.log(e);
+    }
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  return token;
 }
 
 const styles = StyleSheet.create({
--- app/(tabs)/Visits/index.js
+++ app/(tabs)/Visits/index.js
@@ -9,54 +9,12 @@ import { createMaterialTopTabNavigator } from "@react-navigation/material-top-ta
 
 import { useAuth } from "../../../context/AuthContext";
 
-import * as Device from "expo-device";
-import * as Notifications from "expo-notifications";
-
 import { useRouter } from "expo-router";
-
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: true,
-  }),
-});
 
 const Tab = createMaterialTopTabNavigator();
 
 export default function Visits() {
   const { userIsSecurity } = useAuth();
-
-  const [expoPushToken, setExpoPushToken] = useState("");
-  const [notification, setNotification] = useState(false);
-  const notificationListener = useRef();
-  const responseListener = useRef();
-
-  const router = useRouter();
-
-  useEffect(() => {
-    registerForPushNotificationsAsync().then((token) =>
-      setExpoPushToken(token)
-    );
-
-    notificationListener.current =
-      Notifications.addNotificationReceivedListener((notification) => {
-        setNotification(notification);
-      });
-
-    responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((response) => {
-        console.log(response);
-        router.push("visits");
-      });
-
-    return () => {
-      Notifications.removeNotificationSubscription(
-        notificationListener.current
-      );
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
 
   return (
     <Tab.Navigator>
@@ -77,41 +35,4 @@ export default function Visits() {
       )}
     </Tab.Navigator>
   );
-}
-
-async function registerForPushNotificationsAsync() {
-  let token;
-
-  if (Platform.OS === "android") {
-    await Notifications.setNotificationChannelAsync("default", {
-      name: "default",
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: "#FF231F7C",
-    });
-  }
-
-  if (Device.isDevice) {
-    try {
-      const { status: existingStatus } =
-        await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      if (existingStatus !== "granted") {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
-      }
-      if (finalStatus !== "granted") {
-        alert("Failed to get push token for push notification!");
-        return;
-      }
-      token = (await Notifications.getExpoPushTokenAsync()).data;
-      console.log(token);
-    } catch (e) {
-      console.log(e);
-    }
-  } else {
-    alert("Must use physical device for Push Notifications");
-  }
-
-  return token;
 }
